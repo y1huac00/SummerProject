59d1070a7e194d141ff483b5bad874df4485cc05
avangerfamilyhu <jiamianhu@yahoo.com>
2021-06-12 17:50:31 +0800

    Discover the normalization factor

--- Simple_model.py
+++ Simple_model.py
@@ -1,8 +1,11 @@
+import math
 import os
 import pandas as pd
 import torch
 import time
 import copy
+import numpy as np
+import statistics
 from torchvision.io import read_image
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms, models
@@ -10,7 +13,9 @@ from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 from PIL import Image
 import matplotlib.pyplot as plt
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
 
 class CustomImageDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
@@ -24,7 +29,7 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        #image = read_image(img_path)
+        # image = read_image(img_path)
         image = Image.open(img_path)
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
@@ -32,7 +37,6 @@ class CustomImageDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
-
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
@@ -50,7 +54,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             if phase == 'train':
                 model.train()  # Set model to training mode
             else:
-                model.eval()   # Set model to evaluate mode
+                model.eval()  # Set model to evaluate mode
 
             running_loss = 0.0
             running_corrects = 0
@@ -79,8 +83,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
                 cnt += 1
-                if cnt%1000 == 0:
-                    print('finished ', cnt//1000, ' images')
+                if cnt % 1000 == 0:
+                    print('finished ', cnt // 1000, ' images')
             if phase == 'train':
                 scheduler.step()
 
@@ -106,57 +110,95 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     model.load_state_dict(best_model_wts)
     return model
 
+
+# Improvement: change more precise transform normalize parameter
+
+def calculate_norm_value(data_path, src_path):
+    # to be implemented
+    r = []
+    g = []
+    b = []
+    std = [0,0,0]
+    mean = [0,0,0]
+    transform_t = transforms.ToTensor()
+    data_cal = CustomImageDataset(data_path, src_path, transform_t)
+    for i in range(0,data_cal.__len__()):
+        img, _ = data_cal.__getitem__(i)
+        ng = np.array(img)
+        r_ch  = np.reshape(ng[:,:,0],-1)
+        g_ch = np.reshape(ng[:, 0, :], -1)
+        b_ch = np.reshape(ng[0, :, :], -1)
+        r.extend(r_ch)
+        g.extend(g_ch)
+        b.extend(b_ch)
+        std[0] += r_ch.std()
+        mean[0] += r_ch.mean()
+        std[1] += g_ch.std()
+        mean[1] += g_ch.mean()
+        std[2] += b_ch.std()
+        mean[2] += b_ch.mean()
+    r_mean, g_mean, b_mean = np.array(r).mean(), np.array(g).mean(), np.array(b).mean()
+    r_std, g_std, b_std = np.array(r).std(), np.array(g).std(), np.array(b).std()
+    print(r_mean, g_mean, b_mean)
+    print(r_std, g_std, b_std)
+    print(np.array(std)/data_cal.__len__())
+    print(np.array(mean) / data_cal.__len__())
+    print(np.array(std) / math.sqrt(data_cal.__len__()))
+    return 0
+
+'''
+Sample Mean and Std in (r,g,b) from validation set
+    mean: 0.119743854 0.12807578 0.23815322
+    std: 0.113375455 0.112062685 0.22721237
+'''
+
 # Load train, test and validation data by phase. Phase = train, val and test. Target = genus and species
-def load_data(phase,target,d_transfroms,batch_size = 16):
-    data_path = './Metadata/'+target+'_'+phase+'.csv'
+def load_data(phase, target, d_transfroms, batch_size=16):
+    data_path = './Metadata/' + target + '_' + phase + '.csv'
     src_path = './Plaindata'
-    data_out = CustomImageDataset(data_path,src_path,d_transfroms)
+    data_out = CustomImageDataset(data_path, src_path, d_transfroms)
+    calculate_norm_value(data_path, src_path)
     data_size = len(data_out)
+    data_cal, _ = zip(*data_out)
     data_loader = DataLoader(data_out, batch_size=batch_size, shuffle=True)
-    return data_loader,data_size
+    return data_loader, data_size
 
 
+data_transforms = transforms.Compose([transforms.Resize([256, 256]),
+                                      transforms.CenterCrop([224, 224]),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(
+                                          mean=[0.485, 0.456, 0.406],
+                                          std=[0.229, 0.224, 0.225])
+                                      ])
 
-data_transforms = transforms.Compose([transforms.Resize([256,256]),
-        transforms.CenterCrop([224,224]),
-        transforms.ToTensor(),
-        transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225])])
 target = 'genus'
+
 dataloaders = {}
 dataset_sizes = {}
 batch_size: int = 16
-dataloaders['train'], dataset_sizes['train'] = load_data('train', target, data_transforms, batch_size)
+
+#dataloaders['play'], dataset_sizes['play'] = load_data('play', target, data_transforms, batch_size)
+
 dataloaders['val'], dataset_sizes['val'] = load_data('val', target, data_transforms, batch_size)
-#dataloaders['test'], dataset_sizes['test'] = load_data('test', target, data_transforms, batch_size)
+# dataloaders['train'], dataset_sizes['train'] = load_data('train', target, data_transforms, batch_size)
 
-print(dataset_sizes)
+# dataloaders['test'], dataset_sizes['test'] = load_data('test', target, data_transforms, batch_size)
 
-train_features, train_labels = next(iter(dataloaders['train']))
-print(f"Feature batch shape: {train_features.size()}")
-print(f"Labels batch shape: {train_labels.size()}")
+# print(dataset_sizes)
+# train_features, train_labels = next(iter(dataloaders['train']))
+# print(f"Feature batch shape: {train_features.size()}")
+# print(f"Labels batch shape: {train_labels.size()}")
 
-img = train_features[0].squeeze()
-label = train_labels[0]
-unloader = transforms.ToPILImage()
-image = unloader(img)
-plt.imshow(image)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model_ft = models.resnet18(pretrained=True)
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = torch.nn.Linear(num_ftrs, 15)
-
 model_ft = model_ft.to(device)
-
 criterion = torch.nn.CrossEntropyLoss()
-
 # Observe that all parameters are being optimized
 optimizer_ft = torch.optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
-model_conv = train_model(model_ft, criterion, optimizer_ft,
-                         exp_lr_scheduler, num_epochs=5)
+# model_conv = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=5)
