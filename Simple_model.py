d76b6563f3db1dc6db26b181514b768df57f2ef0
Windwosshit <jiamianh@yahoo.com>
2021-06-23 23:03:05 +0800

    ResNet50 tested

--- Simple_model.py
+++ Simple_model.py
@@ -147,8 +147,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
 model_ft = models.resnet50(pretrained=True)
-for param in model_ft.parameters():
-    param.requires_grad = False
+
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = torch.nn.Linear(num_ftrs, 31)
 
@@ -157,10 +156,10 @@ model_ft = model_ft.to(device)
 criterion = torch.nn.CrossEntropyLoss()
 
 # Observe that all parameters are being optimized
-optimizer_ft = torch.optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-
+#optimizer_ft = torch.optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+optimizer_ft = torch.optim.Adam(model_ft.parameters(), lr=0.001)
 # Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=5, gamma=0.1)
 
 model_conv = train_model(model_ft, criterion, optimizer_ft,
                          exp_lr_scheduler, num_epochs=25)
