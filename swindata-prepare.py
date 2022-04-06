b119d54a2cba5796e95d5a61a5b6f3254b119c63
AllenChen <yihuac00@gmail.com>
2022-04-07 01:18:09 +0800

    add swindata-prepare.py

--- swindata-prepare.py
+++ swindata-prepare.py
@@ -19,13 +19,13 @@ def swindataset(path_metadata, path_plaindata, train, val, test, dst):
     dfs = {j:pd.read_csv(os.path.join(path_metadata,i),names=['filename','label'],header=None) for i,j in zip([train, val, test],['train', 'val', 'test'])}
     os.makedirs(dst, exist_ok=True)
     for key in dfs:
-        # for img in dfs[key]['filename'].tolist():
-        #     os.makedirs(os.path.join(dst, key), exist_ok=True)
-        #     shutil.copyfile(src=os.path.join(path_plaindata, img), dst=os.path.join(dst, f'{key}/', img))
-        #     print(img)
-        #
-        # shutil.make_archive(os.path.join(dst,key), 'zip', dst, key)
-        # shutil.rmtree(os.path.join(dst,key))
+        for img in dfs[key]['filename'].tolist():
+            os.makedirs(os.path.join(dst, key), exist_ok=True)
+            shutil.copyfile(src=os.path.join(path_plaindata, img), dst=os.path.join(dst, f'{key}/', img))
+            print(img)
+
+        shutil.make_archive(os.path.join(dst,key), 'zip', dst, key)
+        shutil.rmtree(os.path.join(dst,key))
 
         dfs[key].to_csv(os.path.join(dst,f'{key}_map.txt'), header=None, index=None, sep=' ')
 
