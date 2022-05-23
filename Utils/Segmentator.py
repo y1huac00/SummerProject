19d0504e17810bcdfd3af9e4c6e24bf9633249e8
AllenChen <yihuac00@gmail.com>
2022-05-23 19:25:50 +0800

    update from upstream

--- Utils/Segmentator.py
+++ Utils/Segmentator.py
@@ -29,6 +29,7 @@ def axis_candidate(projection):
     threshold = projection.max() * 0.5
     axis_candidates = []
     for p in range(0, projection.size):
+        print(projection[p], threshold)
         if projection[p] <= threshold:
             axis_candidates.append(p)
     return axis_candidates
@@ -41,8 +42,11 @@ def clean_candidates(candidates, projections, if_x=1):
     else:
         axis_factor = 5
     break_length = projections.size / axis_factor
+    print(break_length, projections.size, axis_factor)
     for c in range(1, len(candidates)):
+        print(candidates[c], candidates[c - 1], candidates[c] - candidates[c - 1])
         if candidates[c] - candidates[c - 1] > break_length / 3:
+
             axis_breaks.append(candidates[c - 1])
             axis_breaks.append(candidates[c])
     return axis_breaks
@@ -74,10 +78,16 @@ def sep_image(img_file, img_folder, thr_value=160, scale=32):
     img, scaled = read_and_down(os.path.join(img_folder, img_file), scale)
 
     thr = cv2.threshold(scaled, thr_value, 255, cv2.THRESH_BINARY_INV)[1]
+    cv2.imshow('thre', thr)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
     h_proj = thr.sum(axis=1)
     w_proj = thr.sum(axis=0)
+    # print(h_proj, w_proj)
     cnd_x = clean_candidates(axis_candidate(w_proj), w_proj, 1)
     cnd_y = clean_candidates(axis_candidate(h_proj), h_proj, 0)
+
+    print(cnd_x, cnd_y)
 
     root_folder = os.path.join(img_folder, img_root)
     if not os.path.exists(root_folder):
@@ -87,11 +97,11 @@ def sep_image(img_file, img_folder, thr_value=160, scale=32):
     #     return
     file_string = os.path.join(root_folder, (img_root+'_grid_'))
 
-    grid_crop(cnd_x, cnd_y, img, file_string, scale)
+    # grid_crop(cnd_x, cnd_y, img, file_string, scale)
 
 
 if __name__=='__main__':
-    img_folder = 'E:/HKU_Study/PhD/Lab_work/Keyence_Images'
+    img_folder = 'D:/pythonproject/ostracod/test'
     for file in files(img_folder):
         print(file)
         sep_image(file, img_folder, 160, 16)
